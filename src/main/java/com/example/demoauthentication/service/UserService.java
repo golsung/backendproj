c2069f174ce397cd0179c4b54e0bf0b93ba4001d
golsung <golsung@naver.com>
2022-01-06 18:42:40 +0900

    first backend code

---
+++ src/main/java/com/example/demoauthentication/service/UserService.java
@@ -0,0 +1,33 @@
+package com.example.demoauthentication.service;
+
+import com.example.demoauthentication.model.UserEntity;
+import com.example.demoauthentication.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserEntity create(final UserEntity userEntity) {
+        if(userEntity == null || userEntity.getEmail() == null ) {
+            throw new RuntimeException("Invalid arguments");
+        }
+        final String email = userEntity.getEmail();
+        System.out.println(email);
+        if(userRepository.existsByEmail(email)) {
+            log.warn("Email already exists {}", email);
+            throw new RuntimeException("Email already exists");
+        }
+
+        return userRepository.save(userEntity);
+    }
+
+    public UserEntity getByCredentials(final String email, final String password) {
+        return userRepository.findByEmailAndPassword(email, password);
+    }
+}
